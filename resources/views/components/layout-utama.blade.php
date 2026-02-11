b6f37c8d2a6b15feafd2c01d6e17b50e344db26b
awkcjs <alikagazella@gmail.com>
2026-02-11 20:26:23 +0700

    add

--- resources/views/components/layout-utama.blade.php
+++ resources/views/components/layout-utama.blade.php
@@ -1,20 +1,22 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Bowlé</title>
-        <link rel="stylesheet" href="../../public/assets/style.css" />
-    </head>
 
-    <body>
-        <header>
-            <h2>Bowlé</h2>
-            <x-navbar></x-navbar>
-        </header>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bowlé</title>
+    <link rel="stylesheet" href="../../public/assets/style.css" />
+</head>
 
-        {{$slot}}
-        <x-footer></x-footer>
-        <script src="../../public/assets/script.js"></script>
-    </body>
+<body>
+    <header>
+        <h2>Bowlé</h2>
+        <x-navbar></x-navbar>
+    </header>
+
+    {{ $slot }}
+    <x-footer></x-footer>
+    <script src="../../public/assets/script.js"></script>
+</body>
+
 </html>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav>
-                <a href="./home.html">Home</a>
-                <a href="./about.html">About</a>
-                <a href="./product.html">Our Menu</a>
-                <a href="./contact.html">Contact</a>
-            </nav>
+    <a href="./home.html">Home</a>
+    <a href="./about.html">About</a>
+    <a href="./product.html">Our Menu</a>
+    <a href="./contact.html">Contact</a>
+</nav>
